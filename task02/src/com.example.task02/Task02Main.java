aa0b7a617f473b7fec4412ca7bfdc2d354ac1614
alexd <alexd@z-inc.net>
2018-07-19 10:45:12 +0500

    Задание 02 Времена года if

---
+++ task02/src/com.example.task02/Task02Main.java
@@ -0,0 +1,15 @@
+package com.example.task02;
+
+public class Task02Main {
+    public static void main(String[] args) {
+        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
+        // например вот так:
+        /*
+        System.out.println(getSeason(12));
+         */
+    }
+
+    static String getSeason(int mothNumber) {
+        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+    }
+}
