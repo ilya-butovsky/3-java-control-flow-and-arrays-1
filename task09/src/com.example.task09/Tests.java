9437fa7678ee5ea576880d3666c0293424d92807
drxaos <vladimir.p.polyakov@gmail.com>
2019-03-09 13:39:57 +0500

    тест в 3.9 на изменение входного массива

--- task09/src/com.example.task09/Tests.java
+++ task09/src/com.example.task09/Tests.java
@@ -15,5 +15,18 @@ public class Tests {
         Assert.assertEquals("min [2147483647] = 2147483647", 2147483647, Task09Main.min(new int[]{2147483647}));
 
     }
-    
+
+    /**
+     * Обычно по сигнатуре или документации метода можно понять, будет ли этот метод производить модификацию аргументов.
+     * Метод _поиска_ минимального значения не подразумевает его изменения.
+     */
+    @Test
+    public void testModification() {
+        int[] originalArray = {5, 4, 7, 2, 8, 3, 6};
+        int[] argumentArray = originalArray.clone();
+        Assert.assertEquals("min [5, 4, 7, 2, 8, 3, 6] = 2", 2, Task09Main.min(argumentArray));
+        Assert.assertArrayEquals("Переданный в метод массив был изменен", originalArray, argumentArray);
+
+    }
+
 }
